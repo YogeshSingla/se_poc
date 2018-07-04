e47012e1829dcd0bca74874658036086706e8e22
yogesh singla <yogeshsingla131@gmail.com>
2018-07-04 08:11:51 -0400

    comment sample code

--- chatbot_demo/mycroft_chatbot_demo.py
+++ chatbot_demo/mycroft_chatbot_demo.py
@@ -1,10 +1,8 @@
-__author__ = 'the-kid89'
+__author__ = 'the-kid89, kirito'
 """
-A sample program that uses multiple intents and disambiguates by
-intent confidence
-try with the following:
-PYTHONPATH=. python examples/multi_intent_parser.py "what's the weather like in tokyo"
-PYTHONPATH=. python examples/multi_intent_parser.py "play some music by the clash"
+A sample program for with hardcoded quiz questions and answers
+that are parsed using intent tones based on given samples into
+positive, negative and neutral.
 """
 
 import json
@@ -24,92 +22,92 @@ parser = Parser(tokenizer, tagger)
 
 engine = DomainIntentDeterminationEngine()
 
-engine.register_domain('Domain1')
-engine.register_domain('Domain2')
-
-# define vocabulary
-weather_keyword = [
-    "weather"
-]
-
-for wk in weather_keyword:
-    engine.register_entity(wk, "WeatherKeyword", domain='Domain1')
-
-weather_types = [
-    "snow",
-    "rain",
-    "wind",
-    "sleet",
-    "sun"
-]
-
-for wt in weather_types:
-    engine.register_entity(wt, "WeatherType", domain='Domain1')
-
-locations = [
-    "Seattle",
-    "San Francisco",
-    "Tokyo"
-]
-
-for l in locations:
-    engine.register_entity(l, "Location", domain='Domain1')
-
-# structure intent
-weather_intent = IntentBuilder("WeatherIntent")\
-    .require("WeatherKeyword")\
-    .optionally("WeatherType")\
-    .require("Location")\
-    .build()
-
-
-# define music vocabulary
-artists = [
-    "third eye blind",
-    "the who",
-    "the clash",
-    "john mayer",
-    "kings of leon",
-    "adelle"
-]
-
-for a in artists:
-    engine.register_entity(a, "Artist", domain='Domain2')
-
-music_verbs = [
-    "listen",
-    "hear",
-    "play"
-]
-
-for mv in music_verbs:
-    engine.register_entity(mv, "MusicVerb", domain='Domain2')
-
-music_keywords = [
-    "songs",
-    "music"
-]
-
-for mk in music_keywords:
-    engine.register_entity(mk, "MusicKeyword", domain='Domain2')
-
-music_intent = IntentBuilder("MusicIntent")\
-    .require("MusicVerb")\
-    .optionally("MusicKeyword")\
-    .optionally("Artist")\
-    .build()
-
-engine.register_intent_parser(weather_intent, domain='Domain1')
-engine.register_intent_parser(music_intent, domain='Domain2')
-
-
-#create a default reply
-engine.register_domain('DefaultDomain')
-default_reply = [ "I do not understand", "Sorry! I didn't get that", "I think I am not programmed for that yet", "Still in beta"]
-for sample in default_reply:
-    engine.register_entity(sample,"DefaultReply", domain='DefaultDomain')
-default_intent = IntentBuilder("DefaultIntent").require("MusicVerb").build()
-engine.register_intent_parser(default_intent, domain='DefaultDomain')
+# engine.register_domain('Domain1')
+# engine.register_domain('Domain2')
+#
+# # define vocabulary
+# weather_keyword = [
+#     "weather"
+# ]
+#
+# for wk in weather_keyword:
+#     engine.register_entity(wk, "WeatherKeyword", domain='Domain1')
+#
+# weather_types = [
+#     "snow",
+#     "rain",
+#     "wind",
+#     "sleet",
+#     "sun"
+# ]
+#
+# for wt in weather_types:
+#     engine.register_entity(wt, "WeatherType", domain='Domain1')
+#
+# locations = [
+#     "Seattle",
+#     "San Francisco",
+#     "Tokyo"
+# ]
+#
+# for l in locations:
+#     engine.register_entity(l, "Location", domain='Domain1')
+#
+# # structure intent
+# weather_intent = IntentBuilder("WeatherIntent")\
+#     .require("WeatherKeyword")\
+#     .optionally("WeatherType")\
+#     .require("Location")\
+#     .build()
+#
+#
+# # define music vocabulary
+# artists = [
+#     "third eye blind",
+#     "the who",
+#     "the clash",
+#     "john mayer",
+#     "kings of leon",
+#     "adelle"
+# ]
+#
+# for a in artists:
+#     engine.register_entity(a, "Artist", domain='Domain2')
+#
+# music_verbs = [
+#     "listen",
+#     "hear",
+#     "play"
+# ]
+#
+# for mv in music_verbs:
+#     engine.register_entity(mv, "MusicVerb", domain='Domain2')
+#
+# music_keywords = [
+#     "songs",
+#     "music"
+# ]
+#
+# for mk in music_keywords:
+#     engine.register_entity(mk, "MusicKeyword", domain='Domain2')
+#
+# music_intent = IntentBuilder("MusicIntent")\
+#     .require("MusicVerb")\
+#     .optionally("MusicKeyword")\
+#     .optionally("Artist")\
+#     .build()
+#
+# engine.register_intent_parser(weather_intent, domain='Domain1')
+# engine.register_intent_parser(music_intent, domain='Domain2')
+#
+#
+# #create a default reply
+# engine.register_domain('DefaultDomain')
+# default_reply = [ "I do not understand", "Sorry! I didn't get that", "I think I am not programmed for that yet", "Still in beta"]
+# for sample in default_reply:
+#     engine.register_entity(sample,"DefaultReply", domain='DefaultDomain')
+# default_intent = IntentBuilder("DefaultIntent").require("MusicVerb").build()
+# engine.register_intent_parser(default_intent, domain='DefaultDomain')
 
 #positive reply intent
 positive_reply = [
@@ -117,7 +115,8 @@ positive_reply = [
     "very often",
     "many",
     "yeah",
-    "ya"
+    "ya",
+    "often"
 ]
 for sample in positive_reply:
     engine.register_entity(sample,"PositiveReplySamples")
@@ -128,6 +127,10 @@ engine.register_intent_parser(positive_intent)
 #negative reply intent
 negative_reply = [
     "no",
+    "hardly",
+    "never",
+    "don't recall",
+    "dont recall",
     "rarely",
     "seldom",
     "not often",
@@ -156,8 +159,18 @@ for sample in neutral_reply:
 neutral_intent = IntentBuilder("NeutralIntent").require("NeutralReplySamples").build()
 engine.register_intent_parser(neutral_intent)
 
+time_span = [
+    "minutes",
+    "hours",
+    "days",
+    "weeks",
+    "months",
+    "years"
+]
 engine.register_regex_entity("(?P<frequency>[0-9]+)")
-quantitative_intent = IntentBuilder("QuantitativeIntent").require("frequency").build()
+for sample in time_span:
+    engine.register_entity(sample,"TimeSpan")
+quantitative_intent = IntentBuilder("QuantitativeIntent").require("frequency").optionally("TimeSpan").build()
 engine.register_intent_parser(quantitative_intent)
 
 #questions
